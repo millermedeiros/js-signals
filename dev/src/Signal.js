677a57bda47862c899aba16e5ec758e76bcc955f
Miller Medeiros <miller@millermedeiros.com>
2010-11-26 03:50:34 -0500

    removed SignalBindingList, refactored code, improved unit tests.

--- dev/src/Signal.js
+++ dev/src/Signal.js
@@ -2,34 +2,49 @@
 	/**
 	 * Simple Signal "Class" inpired by Robert Penner's AS3Signals <https://github.com/robertpenner/as3-signals/>
 	 * @author Miller Medeiros
-	 * @version 0.1 (2010/11/24)
 	 * @constructor
 	 */
 	signals.Signal = function(){
 		/** 
 		 * Event Handlers
-		 * @type signals.BindingList
+		 * @type Array.<SignalBinding>
+		 * @private
 		 */
-		this._bindingList = new signals.SignalBindingList();
+		this._bindings = [];
 	};
 	
 	
 	signals.Signal.prototype = {
 		
 		_registerListener : function(listener, isOnce, scope){
-			var list = this._bindingList,
-				prevIndex = list.indexOfListener(listener),
+			var prevBinding,
+				prevIndex = this._indexOfListener(listener),
 				binding;
 			
 			if(prevIndex != -1){ //avoid creating a new Binding if already added to list
-				//TODO: throw errors based on isOnce
-				binding = list[prevIndex];
+				prevBinding = this._bindings[prevIndex];
+				
+				if(prevBinding.isOnce() && !isOnce){
+					throw new Error('You cannot addOnce() then add() the same listener without removing the relationship first.');
+				}else if(!prevBinding.isOnce() && isOnce){
+					throw new Error('You cannot add() then addOnce() the same listener without removing the relationship first.');
+				}
+				
+				binding = prevBinding;
 			} else {
 				binding = new signals.SignalBinding(listener, isOnce, scope, this);
-				list.add(binding);
+				this._bindings.push(binding);
 			}
 			
 			return binding;
+		},
+		
+		_indexOfListener : function _indexOfListener(listener){
+			var n = this._bindings.length;
+			while(n--){
+				if(this._bindings[n].listener === listener) return n;
+			}
+			return -1;
 		},
 		
 		add : function(listener, scope){
@@ -41,21 +56,30 @@
 		},
 		
 		remove : function remove(listener){
-			this._bindingList.removeByListener(listener);
+			var i = this._indexOfListener(listener);
+			if(i != -1){
+				this._bindings.splice(i, 1);
+			}
 			return listener;
 		},
 		
 		removeAll : function removeAll(){
-			this._bindingList.removeAll();
+			this._bindings.length = 0;
 		},
 		
 		getNumListeners : function getNumListeners(){
-			return this._bindingList.length;
+			return this._bindings.length;
 		},
 		
 		dispatch : function(){
-			var params = Array.prototype.slice.call(arguments);
-			this._bindingList.execute(params);
+			var paramsArr = Array.prototype.slice.call(arguments),
+				i = 0,
+				bindings = this._bindings.slice(), //clone array in case add/remove items during dispatch
+				cur;
+			while(cur = bindings[i++]){
+				cur.execute(paramsArr);
+			}
+			
 		},
 		
 		toString : function toString(){
